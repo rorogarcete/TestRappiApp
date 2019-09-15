39cafd7fb8b24445df8e9320a2cd24a02fc8e582
rorogarcete <oneosmar89@gmail.com>
2019-09-15 01:34:41 -0400

    Add data response for Genre

--- app/src/main/java/com/prestosoftware/test/rappi/data/api/GenreResponse.kt
+++ app/src/main/java/com/prestosoftware/test/rappi/data/api/GenreResponse.kt
@@ -1,6 +1,6 @@
 package com.prestosoftware.test.rappi.data.api
 
-import com.prestosoftware.test.rappi.data.Genre
+import com.prestosoftware.test.rappi.data.model.Genre
 
 data class GenreResponse (
     val genres: List<Genre>
