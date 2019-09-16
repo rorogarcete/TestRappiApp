a3a54c5334709ea0b8b7c8cae70bcba96b470a46
rorogarcete <oneosmar89@gmail.com>
2019-09-15 22:30:33 -0400

    Add utility class for databinding

---
+++ app/src/main/java/com/prestosoftware/test/rappi/ui/binding/FragmentDataBindingComponent.kt
@@ -0,0 +1,14 @@
+package com.prestosoftware.test.rappi.ui.binding
+
+//import androidx.databinding.DataBindingComponent
+//import androidx.databinding.DataBindingComponent
+import androidx.fragment.app.Fragment
+
+/**
+ * A Data Binding Component implementation for fragments.
+ */
+class FragmentDataBindingComponent(fragment: Fragment) { //: DataBindingComponent
+    private val adapter = FragmentBindingAdapters(fragment)
+
+//    override fun getFragmentBindingAdapters() = adapter
+}
