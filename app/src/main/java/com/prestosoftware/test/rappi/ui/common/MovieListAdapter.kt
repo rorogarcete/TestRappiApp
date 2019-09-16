73d87f7a1e85d8f567c8dabdc1fc5fdb6b8a0e72
rorogarcete <oneosmar89@gmail.com>
2019-09-15 22:31:47 -0400

    Add common class for ui. Adapters, ViewHolders and callback retry

---
+++ app/src/main/java/com/prestosoftware/test/rappi/ui/common/MovieListAdapter.kt
@@ -0,0 +1,53 @@
+package com.prestosoftware.test.rappi.ui.common
+
+import androidx.databinding.DataBindingComponent
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.DiffUtil
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import com.prestosoftware.test.rappi.util.AppExecutors
+
+/**
+ * A RecyclerView adapter for [Movie] class.
+ */
+class MovieListAdapter(
+    private val dataBindingComponent: DataBindingComponent,
+    appExecutors: AppExecutors,
+    private val showFullName: Boolean,
+    private val repoClickCallback: ((Repo) -> Unit)?
+) : DataBoundListAdapter<Repo, RepoItemBinding>(
+    appExecutors = appExecutors,
+    diffCallback = object : DiffUtil.ItemCallback<Repo>() {
+        override fun areItemsTheSame(oldItem: Repo, newItem: Repo): Boolean {
+            return oldItem.owner == newItem.owner
+                    && oldItem.name == newItem.name
+        }
+
+        override fun areContentsTheSame(oldItem: Repo, newItem: Repo): Boolean {
+            return oldItem.description == newItem.description
+                    && oldItem.stars == newItem.stars
+        }
+    }
+) {
+
+    override fun createBinding(parent: ViewGroup): RepoItemBinding {
+        val binding = DataBindingUtil.inflate<RepoItemBinding>(
+            LayoutInflater.from(parent.context),
+            R.layout.repo_item,
+            parent,
+            false,
+            dataBindingComponent
+        )
+        binding.showFullName = showFullName
+        binding.root.setOnClickListener {
+            binding.repo?.let {
+                repoClickCallback?.invoke(it)
+            }
+        }
+        return binding
+    }
+
+    override fun bind(binding: RepoItemBinding, item: Repo) {
+        binding.repo = item
+    }
+}
