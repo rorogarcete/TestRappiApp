a11fac785f9d3d5502105779d6baf63ebec93b99
rorogarcete <oneosmar89@gmail.com>
2019-09-16 23:44:30 -0400

    Add extensions class

---
+++ app/src/main/java/com/prestosoftware/test/rappi/util/extension/BindingExtension.kt
@@ -0,0 +1,16 @@
+package com.prestosoftware.test.rappi.util.extension
+
+import android.view.View
+import com.prestosoftware.test.rappi.models.Resource
+import com.prestosoftware.test.rappi.models.Status
+import org.jetbrains.anko.toast
+
+fun View.bindResource(resource: Resource<Any>?, onSuccess: () -> Unit) {
+  if (resource != null) {
+    when (resource.status) {
+      Status.LOADING -> Unit
+      Status.SUCCESS -> onSuccess()
+      Status.ERROR -> this.context.toast(resource.errorEnvelope?.status_message.toString())
+    }
+  }
+}
